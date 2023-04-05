7ca05f6e76ba4243cbc14601fd4829972be11e31
shahzad-35 <shahzadhere17@gmail.com>
2023-04-05 14:50:54 +0500

    added font awesome library added bootstrap added eye to show and hide password

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount('#app')
