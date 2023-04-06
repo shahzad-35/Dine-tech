bbe02c770e4ced0a911eafa84645e02db9cfd654
shahzad-35 <shahzadhere17@gmail.com>
2023-04-06 14:30:43 +0500

    added routes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import router from './routes';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App)
     .component("font-awesome-icon", FontAwesomeIcon)
+    .use(router)
     .mount('#app')
